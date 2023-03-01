6edd584540a80ec9889ef01b6a3622a787ce1c55
mamun060 <almamun.raju520@gmail.com>
2023-03-01 13:49:18 +0600

    solve leetcode 74  problem

--- searching/leetcode/problems34.js
+++ searching/leetcode/problems34.js
@@ -1,31 +1,31 @@
-var searchRange = function (nums, tar) {
-    let l = 0;
-    let r = nums.length - 1;
+var searchRange = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
     let result = [-1, -1];
     let mid;
 
-    while (l <= r) {
-      mid = Math.floor(l / 2 + r / 2);
-      nums[mid] < tar ? (l = mid + 1) : (r = mid - 1);
+    while ( start <= end ) {
+      mid = Math.floor( start / 2 + end / 2);
+      nums[mid] < target ? (start = mid + 1) : ( end = mid - 1);
     }
 
-    if (nums[l] !== tar) {
+    if (nums[start] !== target) {
         return result
     }
 
-    result[0] = l;
-    r = nums.length - 1;
+    result[0] = start;
+    end = nums.length - 1;
 
-    while (l <= r) {
-      const mid = Math.floor(l / 2 + r / 2);
-      if(nums[mid] <= tar ){
-         l = mid + 1;
+    while (start <= end) {
+      const mid = Math.floor( start / 2 + end / 2);
+      if(nums[mid] <= target ){
+         start = mid + 1;
       } else {
-        r = mid - 1
+        end = mid - 1
       }
     }
 
-    result[1] = r;
+    result[1] = end;
     return result;
 };
 
