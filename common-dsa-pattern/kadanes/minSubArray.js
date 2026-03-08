e608831706090393040ca31b34aa9b88ef86d824
Al  Mamun <mamun@smartfieldservice.com>
2026-03-08 15:38:23 +0600

    update the min sub array

--- common-dsa-pattern/kadanes/minSubArray.js
+++ common-dsa-pattern/kadanes/minSubArray.js
@@ -1,7 +1,9 @@
 function minSubArray(arr){
     let minSubArray = arr[0];
+    let globalMin = arr[0];
     for(let i = 0; i < arr.length; i++){
-        minSubArray = Math.min(arr[i], minSubArray + arr[i]);
+        globalMin = Math.min(arr[i], globalMin + arr[i]);
+        minSubArray = Math.min(minSubArray, globalMin);
     }
     return minSubArray;
 }
